dd891dfcb1e9921dbe1ad042339e8d3aa851f4f6
nour-beyrem <53348004+nour-beyrem@users.noreply.github.com>
2022-02-14 23:16:09 +0100

    typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { HelmetMiddleware } from '@nest-middlewares/helmet';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirstMiddleware } from './middlewares/first.middleware';
 import { SecondMiddleware } from './middlewares/second.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'pssi',
+      entities: ['dist/**/*.entity.{js,ts}'],
+      synchronize: true,
+      logging: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
